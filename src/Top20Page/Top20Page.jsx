666be9d35184e736eb46a5a9dd3561121c74e155
redocaksab <Aleksandrastrelchuk62@gmail.com>
2021-10-08 19:32:34 +0300

    added preloader

--- src/Top20Page/Top20Page.jsx
+++ src/Top20Page/Top20Page.jsx
@@ -1,12 +1,17 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Top20Page.module.css";
-
+import preloader from "./../Preloader/preloader.gif";
+import { useState } from 'react';
 
 const Top20Page = (props) => {
+    let [isLoad, setIsLoad] = useState(true);
     let top = props.top.map((item, index) => {
         return (
             <div className={styles.topCategory}>
-                <NavLink to={`/films/${item.id}`} className={styles.poster}><img src={`https://image.tmdb.org/t/p/original${item.poster}`} /></NavLink>
+                <NavLink to={`/films/${item.id}`} className={styles.poster}>
+                    {(isLoad) ? <img src={preloader} onLoad={() => { setIsLoad(false) }} /> :
+                        <img src={`https://image.tmdb.org/t/p/original${item.poster}`} />}
+                </NavLink>
                 <div className={styles.filmName}><b>{index + 1}.</b> {item.title}</div>
             </div>
         )
